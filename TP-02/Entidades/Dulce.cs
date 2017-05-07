d9aa5f5a168963f25ec2e450e3078e96c274f47b
Kriztian Rammstein <Kriztian Rammstein>
2017-05-07 19:38:46 -0300

    Trabajo Practico 2 subido 07-05-2017

---
+++ TP-02/Entidades/Dulce.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        private short _CantidadCalorias;
+
+        public Dulce(EMarca marca, string codigoDeBarras, ConsoleColor color) : base(codigoDeBarras, marca, color)
+        {
+            this._CantidadCalorias = 80;
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return this._CantidadCalorias;
+            }
+            set
+            {
+                this._CantidadCalorias = value;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Snacks : Producto
+    {
+        private short _cantidadCalorias;
+
+
+
+        public Snacks(EMarca marca, string codigoDeBarras, ConsoleColor color) : base(codigoDeBarras, marca, color)
+        {
+            this._cantidadCalorias = 104;
+        }
+
+
+
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return this._cantidadCalorias;
+            }
+            set
+            {
+                this._cantidadCalorias = value;
+            }
+        }
+
+
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
